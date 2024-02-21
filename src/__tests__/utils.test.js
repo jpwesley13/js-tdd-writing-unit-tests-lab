c8a9195d7e31a8547086149cfb341f97d2ec3d76
Jared Patterson <jpwesley34@gmail.com>
2024-02-21 09:57:08 -0800

    makes basic palindrome tests to solve.

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,15 @@
 // Your tests here
+import {isPalindrome} from "../utils";
+
+describe("isPalindrome", () => {
+    it("doubles points for palindromes", () => {
+        const word1 = "racecar";
+        const word2 = "airship";
+
+        const palindrome = isPalindrome(word1);
+        const normal = isPalindrome(word2);
+
+        expect(palindrome).toBe(true);
+        expect(normal).toBe(false)
+    })
+})
